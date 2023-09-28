7ea72da356f2fb36c3ec7b4224c3f0bdc8b57ab0
Eugene Makarihkin <eugene@makarikhin.info>
2023-09-28 21:17:00 +0200

    Added material from lesson 2.1

---
+++ courses/01_go_web_course/02_routing/01_simple_routing/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "net/http"
+
+type MyHandler struct{}
+
+func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		w.Write([]byte("Home\n"))
+		return
+	}
+	if r.URL.Path == "/hello" {
+		w.Write([]byte("Hello user\n"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Page Not found"))
+}
+
+func main() {
+	err := http.ListenAndServe(":3000", MyHandler{})
+	if err != nil {
+		panic(err)
+	}
+}
