edb158c4e8f624495bc85a203f6ac5d70cc58a0a
Eugene Makarikhin <contact@aboutslack.info>
2024-05-22 10:30:56 +0200

    Added material from tutorial 6

--- rrouge/level.go
+++ rrouge/level.go
@@ -24,6 +24,7 @@ type MapTile struct {
 // NewLevel creates a new game level in a dungeon.
 func NewLevel() Level {
 	l := Level{}
+
 	rooms := make([]Rect, 0)
 	l.Rooms = rooms
 	l.GenerateLevelTiles()
@@ -35,8 +36,8 @@ func NewLevel() Level {
 func (level *Level) DrawLevel(screen *ebiten.Image) {
 	gd := NewGameData()
 
-	for x := 0; x < gd.ScreenWidth; x += 1 {
-		for y := 0; y < gd.ScreenHeight; y += 1 {
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
 
 			tile := level.Tiles[level.GetIndexFromXY(x, y)]
 			op := &ebiten.DrawImageOptions{}
@@ -64,12 +65,13 @@ func (level *Level) GenerateLevelTiles() {
 	gd := NewGameData()
 	tiles := level.createTiles()
 	level.Tiles = tiles
+	contains_rooms := false
 
 	for idx := 0; idx < MAX_ROOMS; idx++ {
 		w := GetRandomBetween(MIN_SIZE, MAX_SIZE)
 		h := GetRandomBetween(MIN_SIZE, MAX_SIZE)
-		x := GetDiceRoll(gd.ScreenWidth-w-1) - 1
-		y := GetDiceRoll(gd.ScreenHeight-h-1) - 1
+		x := GetDiceRoll(gd.ScreenWidth - w - 1)
+		y := GetDiceRoll(gd.ScreenHeight - h - 1)
 		new_room := NewRect(x, y, w, h)
 
 		okToAdd := true
@@ -82,11 +84,55 @@ func (level *Level) GenerateLevelTiles() {
 		}
 		if okToAdd {
 			level.createRoom(new_room)
-			level.Rooms = append(level.Rooms, new_room)
+			if contains_rooms {
+				newX, newY := new_room.Center()
+				prevX, prevY := level.Rooms[len(level.Rooms)-1].Center()
+				coinflip := GetDiceRoll(2)
+				if coinflip == 2 {
+					level.createHorizontalTunnel(prevX, newX, prevY)
+					level.createVerticalTunnel(prevY, newY, newX)
 
+				} else {
+					level.createHorizontalTunnel(prevX, newX, newY)
+					level.createVerticalTunnel(prevY, newY, prevX)
+				}
+
+			}
+
+			level.Rooms = append(level.Rooms, new_room)
+			contains_rooms = true
 		}
 	}
+}
 
+func (level *Level) createHorizontalTunnel(x1 int, x2 int, y int) {
+	gd := NewGameData()
+	for x := min(x1, x2); x < max(x1, x2)+1; x++ {
+		index := level.GetIndexFromXY(x, y)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			level.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			level.Tiles[index].Image = floor
+		}
+	}
+}
+
+func (level *Level) createVerticalTunnel(y1 int, y2 int, x int) {
+	gd := NewGameData()
+	for y := min(y1, y2); y < max(y1, y2)+1; y++ {
+		index := level.GetIndexFromXY(x, y)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			level.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			level.Tiles[index].Image = floor
+		}
+	}
 }
 
 // createTiles creates a map of all walls as a baseline for carving out a level.
@@ -108,15 +154,14 @@ func (level *Level) createTiles() []MapTile {
 				Image:   wall,
 			}
 			tiles[index] = tile
-
 		}
 	}
 	return tiles
 }
 
 func (level *Level) createRoom(room Rect) {
-	for y := room.Y1 + 1; y < room.Y2; y += 1 {
-		for x := room.X1 + 1; x < room.X2; x += 1 {
+	for y := room.Y1 + 1; y < room.Y2; y++ {
+		for x := room.X1 + 1; x < room.X2; x++ {
 			index := level.GetIndexFromXY(x, y)
 			level.Tiles[index].Blocked = false
 			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
@@ -126,4 +171,20 @@ func (level *Level) createRoom(room Rect) {
 			level.Tiles[index].Image = floor
 		}
 	}
+}
+
+// Max returns the larger of x or y.
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+// Min returns the smaller of x or y.
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
 }
