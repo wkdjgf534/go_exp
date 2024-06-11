089e2587f716ba0942846e276c9e0992593ac895
Eugene Makarikhin <contact@aboutslack.info>
2024-06-12 00:00:34 +0200

    Added logger section

---
+++ 02_url-shortener/README.md
@@ -0,0 +1,5 @@
+### URL Shortener
+
+#### How to exuectute the programm
+
+`CONFIG_PATH=./config/local.yaml go run ./cmd/url-shoetener/main.go`
--- 02_url-shortener/cmd/url-shoetener/main.go
+++ 02_url-shortener/cmd/url-shoetener/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
-
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// init config
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// TODO: init logger => slog
+	// TODO: init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	fmt.Println(log)
 	// TODO: init storage => sqlite
 	// TODO: init router => chi, chi/render
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
