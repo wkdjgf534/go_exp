706858eb26b2567c79e0b7c8bba68e80e487d750
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2024-07-17 22:46:27 +0200

    Added material from lesson 2 (#5)

---
+++ 02_go_yt_series/lesson_2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"xor/cipherer"
+)
+
+var mode = flag.String("mode", "cipher", "Set to 'cipher' or 'decipher'. Default is 'cipher'.")
+var secretKey = flag.String("secret", "", "Your secret key. Must contain at least 1 character")
+
+func main() {
+	flag.Parse()
+
+	if len(*secretKey) == 0 {
+		fmt.Fprintln(os.Stderr, "No secret is provided! Exiting now...")
+		os.Exit(1)
+	}
+
+	switch *mode {
+	case "cipher":
+		plaintext := getUserInput("Enter your text to cipher: ")
+		cipheredText, err := cipherer.Cipher(plaintext, *secretKey)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error encrypting text: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(cipheredText)
+	case "decipher":
+		cipheredText := getUserInput("Enter your ciphered data to decipher: ")
+
+		decipheredText, err := cipherer.Decipher(cipheredText, *secretKey)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decrypting text: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(decipheredText)
+	default:
+		fmt.Println("Invalid mode. Use 'chiper' or 'decipher'.")
+		os.Exit(1)
+	}
+}
+
+func getUserInput(msg string) string {
+	fmt.Print(msg)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		result, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occurred while reading the entered text! Please try again...")
+			continue
+		}
+
+		return strings.TrimRight(result, "\n") // "\r\n" for windows
+	}
+
+}
