155f17896a0191f8eab85337429ef9ea0b566bbe
Eugene Makarikhin <contact@aboutslack.info>
2024-10-07 23:51:43 +0200

    Added camera

--- 03_rpg_tutorial/main.go
+++ 03_rpg_tutorial/main.go
@@ -19,6 +19,7 @@ type Game struct {
 	potions     []*entities.Potion
 	tilemapJSON *TilemapJSON
 	tilemapImg  *ebiten.Image
+	cam         *Camera
 }
 
 func (g *Game) Update() error {
@@ -65,6 +66,14 @@ func (g *Game) Update() error {
 
 	}
 
+	g.cam.FollowTarget(g.player.X+8, g.player.Y+8, 320, 240)
+	g.cam.Constrain(
+		float64(g.tilemapJSON.Layers[0].Width)*16.0,
+		float64(g.tilemapJSON.Layers[0].Height)*16.0,
+		320,
+		240,
+	)
+
 	return nil
 }
 
@@ -99,6 +108,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			// set the drawimageoptions to draw the tile at x, y
 			opts.GeoM.Translate(float64(x), float64(y))
 
+			opts.GeoM.Translate(g.cam.X, g.cam.Y)
+
 			// draw the tile
 			screen.DrawImage(
 				// cropping out the tile that we want from the spritesheet
@@ -113,6 +124,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// set the translation of our drawImageOptions to the player's position
 	opts.GeoM.Translate(g.player.X, g.player.Y)
+	opts.GeoM.Translate(g.cam.X, g.cam.Y)
 
 	// draw the player
 	screen.DrawImage(
@@ -127,6 +139,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	for _, sprite := range g.enemies {
 		opts.GeoM.Translate(sprite.X, sprite.Y)
+		opts.GeoM.Translate(g.cam.X, g.cam.Y)
 
 		screen.DrawImage(
 			sprite.Img.SubImage(
@@ -142,6 +155,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	for _, sprite := range g.potions {
 		opts.GeoM.Translate(sprite.X, sprite.Y)
+		opts.GeoM.Translate(g.cam.X, g.cam.Y)
 
 		screen.DrawImage(
 			sprite.Img.SubImage(
@@ -205,34 +219,35 @@ func main() {
 		},
 		enemies: []*entities.Enemy{
 			{
-				&entities.Sprite{
+				Sprite: &entities.Sprite{
 					Img: skeletonImg,
 					X:   100.0,
 					Y:   100.0,
 				},
-				true,
+				FollowsPlayer: true,
 			},
 			{
-				&entities.Sprite{
+				Sprite: &entities.Sprite{
 					Img: skeletonImg,
 					X:   150.0,
 					Y:   50.0,
 				},
-				false,
+				FollowsPlayer: false,
 			},
 		},
 		potions: []*entities.Potion{
 			{
-				&entities.Sprite{
+				Sprite: &entities.Sprite{
 					Img: potionImg,
 					X:   210.0,
 					Y:   100.0,
 				},
-				1.0,
+				AmtHeal: 1.0,
 			},
 		},
 		tilemapJSON: tilemapJSON,
 		tilemapImg:  tilemapImg,
+		cam:         NewCamera(0.0, 0.0),
 	}
 
 	if err := ebiten.RunGame(&game); err != nil {
