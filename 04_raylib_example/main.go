db41c7b2e8ac063061892c326aef43a2ae5daeb2
Eugene Makarikhin <contact@aboutslack.info>
2024-11-14 01:36:42 +0100

    Replaced newCamera func

--- 04_raylib_example/main.go
+++ 04_raylib_example/main.go
@@ -4,6 +4,11 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	maxZoom = 5.0
+	minZoom = 1.0
+)
+
 func main() {
 	screenWidth := int32(800)
 	screenHeight := int32(450)
@@ -14,35 +19,38 @@ func main() {
 
 	player := NewPlayer("TestPlayer", centerVector, 1.5)
 	enemy := NewEnemy("Orc", centerVector, 1.0)
-	camera := NewCamera(centerVector, centerVector, 2.0)
+	camera := rl.NewCamera2D(centerVector, centerVector, 0, 2.0)
 
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
 		movePlayer(player)
-
-		if rl.IsKeyPressed(rl.KeyEqual) && camera.Zoom < maxZoom {
-			camera.Zoom += 0.5
-		}
-		if rl.IsKeyPressed(rl.KeyMinus) && camera.Zoom > minZoom {
-			camera.Zoom -= 0.5
-		}
+		zoomCamera(&camera)
 
 		cursor := rl.GetMousePosition()
-		cursorCamera := rl.GetScreenToWorld2D(cursor, *camera)
+		cursorCamera := rl.GetScreenToWorld2D(cursor, camera)
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.BeginMode2D(*camera)
+		rl.BeginMode2D(camera)
 
-		rl.DrawCircleV(player.position, camera.Zoom, rl.Maroon)
+		rl.DrawCircleV(player.position, camera.Zoom, rl.Black)
 		rl.DrawCircleV(enemy.position, camera.Zoom, rl.Red)
-		rl.DrawCircleLines(int32(cursorCamera.X), int32(cursorCamera.Y), 4, rl.Red)
+		rl.DrawCircleLines(int32(cursorCamera.X), int32(cursorCamera.Y), 2, rl.Red)
 
 		rl.EndMode2D()
 
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
+}
+
+func zoomCamera(camera *rl.Camera2D) {
+	if rl.IsKeyPressed(rl.KeyEqual) && camera.Zoom < maxZoom {
+		camera.Zoom += 0.5
+	}
+	if rl.IsKeyPressed(rl.KeyMinus) && camera.Zoom > minZoom {
+		camera.Zoom -= 0.5
+	}
 }
 
 func movePlayer(player *Player) {
