124b94c5caa00ef52aa7a3f9b2f4c29157b7628a
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2024-11-17 13:49:27 +0100

    raylib-example-3 (#14)
    * Added cursor
    
    * Replaced newCamera func
    
    * Refactored main.go

--- 04_raylib_example/main.go
+++ 04_raylib_example/main.go
@@ -4,49 +4,66 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	screenWidth  = 1000
+	screenHeight = 480
+	maxZoom      = 5.0
+	minZoom      = 1.0
+)
+
 func main() {
-	screenWidth := int32(800)
-	screenHeight := int32(450)
 	rl.InitWindow(800, 450, "Dungeon crawler")
+	rl.HideCursor()
 
 	centerVector := rl.NewVector2(float32(screenWidth)/2, float32(screenHeight)/2)
 
-	player := NewPlayer("TestPlayer", centerVector, 4.5)
-	enemy := NewEnemy(125, "Orc", centerVector, 1.5)
-	camera := NewCamera(centerVector, centerVector, 1.0)
+	player := NewPlayer("TestPlayer", centerVector, 1.5)
+	enemy := NewEnemy("Orc", centerVector, 1.0)
+	camera := rl.NewCamera2D(centerVector, centerVector, 0, 2.0)
 
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
-		if rl.IsKeyDown(rl.KeyRight) {
-			player.position.X += player.speed
-		}
-		if rl.IsKeyDown(rl.KeyLeft) {
-			player.position.X -= player.speed
-		}
-		if rl.IsKeyDown(rl.KeyUp) {
-			player.position.Y -= player.speed
-		}
-		if rl.IsKeyDown(rl.KeyDown) {
-			player.position.Y += player.speed
-		}
+		movePlayer(player)
+		zoomCamera(&camera)
 
-		if rl.IsKeyPressed(rl.KeyEqual) && camera.Zoom < maxZoom {
-			camera.Zoom += 0.5
-		}
-		if rl.IsKeyPressed(rl.KeyMinus) && camera.Zoom > minZoom {
-			camera.Zoom -= 0.5
-		}
+		cursor := rl.GetMousePosition()
+		cursorCamera := rl.GetScreenToWorld2D(cursor, camera)
 
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
-		rl.BeginMode2D(*camera)
+		rl.BeginMode2D(camera)
 
-		rl.DrawCircleV(player.position, camera.Zoom, rl.Maroon)
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawCircleV(player.position, camera.Zoom, rl.Black)
 		rl.DrawCircleV(enemy.position, camera.Zoom, rl.Red)
+		rl.DrawCircleLines(int32(cursorCamera.X), int32(cursorCamera.Y), 2, rl.Red)
 
 		rl.EndMode2D()
-
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
+}
+
+func zoomCamera(camera *rl.Camera2D) {
+	if rl.IsKeyPressed(rl.KeyEqual) && camera.Zoom < maxZoom {
+		camera.Zoom += 0.5
+	}
+	if rl.IsKeyPressed(rl.KeyMinus) && camera.Zoom > minZoom {
+		camera.Zoom -= 0.5
+	}
+}
+
+func movePlayer(player *Player) {
+	if rl.IsKeyDown(rl.KeyRight) {
+		player.position.X += player.speed
+	}
+	if rl.IsKeyDown(rl.KeyLeft) {
+		player.position.X -= player.speed
+	}
+	if rl.IsKeyDown(rl.KeyUp) {
+		player.position.Y -= player.speed
+	}
+	if rl.IsKeyDown(rl.KeyDown) {
+		player.position.Y += player.speed
+	}
 }
