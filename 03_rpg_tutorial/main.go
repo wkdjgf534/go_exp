7398251f6c67080cb7ec4f3556f779cb157d7b24
Eugene Makarikhin <contact@aboutslack.info>
2025-02-15 10:18:17 +0100

    Added material from video 3

--- 03_rpg_tutorial/main.go
+++ 03_rpg_tutorial/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -10,26 +11,69 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct {
-	PlayerImage *ebiten.Image
+type Sprite struct {
+	Img *ebiten.Image
 	X           float64
 	Y           float64
+}
+
+type Player struct {
+	*Sprite
+	Health uint
+}
+
+type Enemy struct {
+	*Sprite
+	FollowsPlayer bool
+}
+
+type Potion struct {
+	*Sprite
+	AmtHeal uint
+}
+
+type Game struct {
+	player *Player
+	enemies []*Enemy
+	potions []*Potion
 }
 
 func (g *Game) Update() error {
 
 	// react to key presses
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.X += 2
+		g.player.X += 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.X -= 2
+		g.player.X -= 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.Y -= 2
+		g.player.Y -= 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.Y += 2
+		g.player.Y += 2
+	}
+
+	for _, sprite := range g.enemies {
+		if sprite.FollowsPlayer {
+			if sprite.X < g.player.X {
+				sprite.X += 0.5
+			} else if sprite.X > g.player.X {
+				sprite.X -= 0.5
+			}
+			if sprite.Y < g.player.Y {
+				sprite.Y += 0.5
+			} else if sprite.Y > g.player.Y {
+				sprite.Y -= 0.5
+			}
+		}
+	}
+
+	for _, potion := range g.potions {
+		if g.player.X > potion.X {
+			g.player.Health += potion.AmtHeal
+			fmt.Printf("Picked up potion! Health: %d\n", g.player.Health )
+		}
 	}
 
 	return nil
@@ -39,13 +83,41 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{120, 180, 255, 255})
 
 	opts := ebiten.DrawImageOptions{}
-	opts.GeoM.Translate(g.X, g.Y)
+	opts.GeoM.Translate(g.player.X, g.player.Y)
 
 	// Draw our player
 	screen.DrawImage(
-		g.PlayerImage.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+		g.player.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
 		&opts,
 	)
+
+	opts.GeoM.Reset()
+
+
+	for _, sprite := range g.enemies {
+		opts.GeoM.Translate(sprite.X, sprite.Y)
+
+		screen.DrawImage(
+			sprite.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+			&opts,
+		)
+
+		opts.GeoM.Reset()
+	}
+
+	//opts.GeoM.Reset()
+
+
+	for _, sprite := range g.potions {
+		opts.GeoM.Translate(sprite.X, sprite.Y)
+
+		screen.DrawImage(
+			sprite.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+			&opts,
+		)
+
+		opts.GeoM.Reset()
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -62,7 +134,56 @@ func main() {
 		log.Fatal()
 	}
 
-	if err := ebiten.RunGame(&Game{PlayerImage: playerImg, X: 100, Y: 100}); err != nil {
+	skeletonImg, _, err := ebitenutil.NewImageFromFile(filepath.Join("assets", "images", "skeleton.png"))
+	if err != nil {
+		log.Fatal()
+	}
+
+	potionImg, _, err := ebitenutil.NewImageFromFile(filepath.Join("assets", "images", "potion.png"))
+	if err != nil {
+		log.Fatal()
+	}
+
+	game := Game{
+		player: &Player {
+			&Sprite {
+				Img: playerImg,
+				X:   50.0,
+				Y:   50.0,
+			},
+			3,
+		},
+		enemies: []*Enemy {
+			{
+				&Sprite {
+					Img: skeletonImg,
+					X:   100.0,
+					Y:   100.0,
+				},
+				true,
+			},
+			{
+				&Sprite {
+					Img: skeletonImg,
+					X:   150.0,
+					Y:   50.0,
+				},
+				false,
+			},
+		},
+		potions: []*Potion {
+			{
+				&Sprite {
+					Img: potionImg,
+					X:   210.0,
+					Y:   100.0,
+				},
+				1.0,
+			},
+		},
+	}
+
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
