ba34c9cdb1bd53f98aa038ec8eea3edf441528c0
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2025-03-05 23:27:36 +0100

    go-newsapi-1 (#32)
    * Added basic REST API server
    
    * Added basic logger stuff

---
+++ 02_newsapi/cmd/api-server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"newsapi/internal/router"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+	logger.Info("server starting on port 8080")
+
+	r := router.New()
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		logger.Error("failed to start server", "error", err)
+	}
+}
