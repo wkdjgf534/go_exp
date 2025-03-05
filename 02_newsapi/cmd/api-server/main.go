858eabecb801aa3919fdd1b454710f09fa5f7295
Eugene Makarikhin <contact@aboutslack.info>
2025-03-05 23:26:10 +0100

    Added basic logger stuff

--- 02_newsapi/cmd/api-server/main.go
+++ 02_newsapi/cmd/api-server/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"newsapi/internal/router"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+	logger.Info("server starting on port 8080")
+
 	r := router.New()
 	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal("failed to start server: ", err)
+		logger.Error("failed to start server", "error", err)
 	}
-
 }
