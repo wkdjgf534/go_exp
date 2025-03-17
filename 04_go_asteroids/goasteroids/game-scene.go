4b1b763607c763878061b139500439a8e952cbe7
Eugene Makarikhin <contact@aboutslack.info>
2025-03-17 23:37:55 +0100

    Added resolv for meteors, comments for several files

--- 04_go_asteroids/goasteroids/game-scene.go
+++ 04_go_asteroids/goasteroids/game-scene.go
@@ -7,22 +7,25 @@ import (
 )
 
 const (
-	baseMeteorVelocity  = 0.25
-	meteorSpawnTime     = 100 * time.Millisecond
-	meteorSpeedUpAmount = 0.1
-	meteorSpeedUpTime   = 1000 * time.Millisecond
+	baseMeteorVelocity  = 0.25                    // The base speed for meteors.
+	meteorSpawnTime     = 100 * time.Millisecond  // How long before meteors spawn.
+	meteorSpeedUpAmount = 0.1                     // How much do we speed a meteor up when it's timer runs out.
+	meteorSpeedUpTime   = 1000 * time.Millisecond // How long to wait to speed up meteors.
 )
 
+// GameScene is the overall type for a game scene (e.g. TitleScene, GameScene, etc.).
 type GameScene struct {
 	player           *Player
-	baseVelocity     float64
-	meteorCount      int
-	meteorSpawnTimer *Timer
-	meteors          map[int]*Meteor
-	meteorsForLevel  int
-	velocityTimer    *Timer
+	baseVelocity     float64         // The base velocity for items in the game.
+	meteorCount      int             // The counter for meteors.
+	meteorSpawnTimer *Timer          // The timer for spawning meteors.
+	meteors          map[int]*Meteor // A map of meteors.
+	meteorsForLevel  int             // # of meteors for a level.
+	velocityTimer    *Timer          // The timer used for speeding up meteors.
 }
 
+// NewGameScene is a factory method for producing a new game. It's called once,
+// when game play starts (and again when game play restarts).
 func NewGameScene() *GameScene {
 	g := &GameScene{
 		meteorSpawnTimer: NewTimer(meteorSpawnTime),
@@ -37,6 +40,7 @@ func NewGameScene() *GameScene {
 	return g
 }
 
+// Update updates all game scene elements for the next draw. It's called once per tick.
 func (g *GameScene) Update(state *State) error {
 	g.player.Update()
 
@@ -51,6 +55,7 @@ func (g *GameScene) Update(state *State) error {
 	return nil
 }
 
+// Draw draws all game scene elements to the screen. It's called once per frame.
 func (g *GameScene) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 
@@ -60,10 +65,12 @@ func (g *GameScene) Draw(screen *ebiten.Image) {
 	}
 }
 
+// Layout is necessary to satisfy interface requirements from ebiten.
 func (g *GameScene) Layout(outsideWidth, outsideHeight int) (ScreenWidth, ScreenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
+// spawnMeteors creates meteors, up to the maximum for a level.
 func (g *GameScene) spawnMeteors() {
 	g.meteorSpawnTimer.Update()
 	if g.meteorSpawnTimer.IsReady() {
@@ -76,6 +83,7 @@ func (g *GameScene) spawnMeteors() {
 	}
 }
 
+// speedUpMeteors makes meteors move faster over time.
 func (g *GameScene) speedUpMeteors() {
 	g.velocityTimer.Update()
 	if g.velocityTimer.IsReady(){
--- 04_go_asteroids/goasteroids/meteor.go
+++ 04_go_asteroids/goasteroids/meteor.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/solarlune/resolv"
 
 	"go-asteroids/assets"
 )
@@ -15,16 +16,19 @@ const (
 	numberOfSmallMeteorsFromLargeMeteor = 4
 )
 
+// Meteor is the type for nall meteors, both big and small.
 type Meteor struct {
-	game          *GameScene
-	position      Vector
-	rotation      float64
-	movement      Vector
-	angle         float64
-	rotationSpeed float64
-	sprite        *ebiten.Image
+	game          *GameScene     // Embed the game so we have access to it.
+	position      Vector         // Where is the meteor.
+	rotation      float64        // The rotation for the meteor.
+	movement      Vector         // What direction is it going.
+	angle         float64        // What angle is it moving at.
+	rotationSpeed float64        // The speed of rotation.
+	sprite        *ebiten.Image  // The image.
+	meteorObj     *resolv.Circle // The collision object.
 }
 
+// NewMeteor is a factory method which creates a new large meteor.
 func NewMeteor(baseVelocity float64, g *GameScene, index int) *Meteor {
 	// Target the center of the screen.
 	target := Vector {
@@ -32,7 +36,7 @@ func NewMeteor(baseVelocity float64, g *GameScene, index int) *Meteor {
 		Y: ScreenHeight/2,
 	}
 
-	// Pick a random angle.
+    // Pick a random angle. 2π is 360°, so this returns an angle between 0° to 360°.
 	angle := rand.Float64() * 2 * math.Pi
 
 	// The distance from the center that meteor should spawn at. Half the width, add some arbitrary distance.
@@ -64,15 +68,23 @@ func NewMeteor(baseVelocity float64, g *GameScene, index int) *Meteor {
 	// Assign a sprite to the meteor.
 	sprite := assets.MeteorSprites[rand.Intn(len(assets.MeteorSprites))]
 
+	// Create the collision object.
+	meteoObj := resolv.NewCircle(pos.X, pos.Y, float64(sprite.Bounds().Dx()/2))
+
 	// Create a meteor object and return it.
 	m := &Meteor{
-		game: g,
-		position: pos,
-		movement: movement,
+		game:          g,
+		position:      pos,
+		movement:      movement,
 		rotationSpeed: rotationSpeedMin + rand.Float64()*(rotationSpeedMax-rotationSpeedMin),
-		sprite: sprite,
-		angle: angle,
+		sprite:        sprite,
+		angle:         angle,
+		meteorObj:     meteoObj,
 	}
+
+	m.meteorObj.SetPosition(pos.X, pos.Y)
+	m.meteorObj.Tags().Set(TagMeteor | TagLarge)
+	m.meteorObj.SetData(&ObjectData{index: index})
 
 	return m
 }
@@ -87,6 +99,7 @@ func (m *Meteor) Update() {
 
 	// Keep meteor on screen.
 	m.keepOnScreen()
+	m.meteorObj.SetPosition(m.position.X, m.position.Y)
 }
 
 func (m *Meteor) Draw(screen *ebiten.Image) {
@@ -107,14 +120,18 @@ func (m *Meteor) Draw(screen *ebiten.Image) {
 func (m *Meteor) keepOnScreen() {
 	if m.position.X >= float64(ScreenWidth) {
 		m.position.X = 0
+		m.meteorObj.SetPosition(0, m.position.Y)
 	}
 	if m.position.X < 0 {
 		m.position.X = ScreenWidth
+		m.meteorObj.SetPosition(ScreenWidth, m.position.Y)
 	}
 	if m.position.Y >= float64(ScreenHeight) {
 		m.position.Y = 0
+		m.meteorObj.SetPosition(m.position.X, 0)
 	}
 	if m.position.Y < 0 {
 		m.position.Y = ScreenHeight
+		m.meteorObj.SetPosition(ScreenHeight, m.position.Y)
 	}
 }
