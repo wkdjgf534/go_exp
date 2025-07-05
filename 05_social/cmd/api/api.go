2b249b75b34403421b691817ed42f32c32cfc4fc
Eugene Makarikhin <contact@aboutslack.info>
2025-07-05 13:54:54 +0200

    Added db, mingrations

--- 05_social/cmd/api/api.go
+++ 05_social/cmd/api/api.go
@@ -7,14 +7,25 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"social/internal/store"
 )
 
 type application struct {
 	config config
+	store  store.Storage
 }
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
--- 05_social/go.mod
+++ 05_social/go.mod
@@ -5,4 +5,5 @@ go 1.24.4
 require (
 	github.com/go-chi/chi/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
