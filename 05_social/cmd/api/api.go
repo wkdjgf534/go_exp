fb394dd9d9459c097765bf51b4cc32059b032b08
Eugene Makarikhin <contact@aboutslack.info>
2025-07-07 18:14:19 +0200

    Added post api

--- 05_social/cmd/api/api.go
+++ 05_social/cmd/api/api.go
@@ -19,6 +19,7 @@ type application struct {
 type config struct {
 	addr string
 	db   dbConfig
+	env  string
 }
 
 type dbConfig struct {
@@ -44,6 +45,14 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
+		})
 	})
 
 	return r
@@ -59,6 +68,5 @@ func (app *application) run(mux http.Handler) error {
 	}
 
 	log.Printf("server has started at %s", app.config.addr)
-
 	return srv.ListenAndServe()
 }
---
+++ 05_social/cmd/api/posts.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"social/internal/store"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type CreatePostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload CreatePostPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		UserID:  1, // TODO: change it
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Posts.Create(ctx, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
