d9362202b144d09c875dba7ab39329de08aed848
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2025-07-13 16:04:54 +0200

    go-social-4 (#51)
    * Added basic comments
    
    * Added post delete and update actions
    
    * Added version for optimistic blocking, seed function

---
+++ 05_social/cmd/migrate/seed/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"social/internal/db"
+	"social/internal/store"
+)
+
+func main() {
+	addr := "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"
+	conn, err := db.New(addr, 30, 30, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	store := store.NewStorage(conn)
+
+	db.Seed(store)
+}
--- 05_social/internal/store/storage.go
+++ 05_social/internal/store/storage.go
@@ -4,25 +4,34 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var (
-	ErrNotFound = errors.New("resource not found")
+	ErrNotFound          = errors.New("resource not found")
+	QueryTimeoutDuration = time.Second * 5
 )
 
 type Storage struct {
 	Posts interface {
 		Create(context.Context, *Post) error
 		GetByID(context.Context, int64) (*Post, error)
+		Delete(context.Context, int64) error
+		Update(context.Context, *Post) error
 	}
 	Users interface {
 		Create(context.Context, *User) error
+	}
+	Comments interface {
+		GetByPostID(context.Context, int64) ([]Comment, error)
+		Create(context.Context, *Comment) error
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostStore{db},
-		Users: &UserStore{db},
+		Posts:    &PostStore{db},
+		Users:    &UserStore{db},
+		Comments: &CommentStore{db},
 	}
 }
