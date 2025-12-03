414590dcbb84d3a41db9f618dadbe551d8696715
Eugene Makarikhin <contact@aboutslack.info>
2025-12-03 22:19:22 +0100

    Added 06_getting_started_with_go/lesson-03

---
+++ 06_getting_started_with_go/lesson-03/go.mod
@@ -0,0 +1,3 @@
+module lesson-03
+
+go 1.25.4
---
+++ 06_getting_started_with_go/lesson-03/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variable of type uint8
+	var smallPositiveValue uint8
+	smallPositiveValue = 10
+	fmt.Println(smallPositiveValue)
+
+	// Variable of type int8
+	var smallPositiveNegativeInt int8
+	smallPositiveNegativeInt = -10
+	fmt.Println(smallPositiveNegativeInt)
+
+	// uint16, uint32, uint64
+	// int16, int32, int64
+	var myInt = 230000043
+	fmt.Println(myInt)
+
+	myInt = int(smallPositiveNegativeInt)
+	myInt = int(smallPositiveValue)
+	// to typecase a value int()
+
+	var myByte byte = 'A'
+	fmt.Println(myByte)
+	// byte is mainly use to represent raw data
+	// and also to distinguish between uint8 and byte
+	// since byte is an alias for uint8
+
+	// Rune is an alias for int32
+	var myRune rune = 'B'
+	fmt.Println(myRune)
+}
