3763d1b834beab12f4f1c3e581674c416935c9e9
Eugene Makarikhin <contact@aboutslack.info>
2025-12-07 11:50:35 +0100

    Added 06_getting_started_with_go/lesson-08

---
+++ 06_getting_started_with_go/lesson-08/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var _myInt int = 10
+	fmt.Println(_myInt)
+
+	var my9Int int = 15
+	fmt.Println(my9Int)
+
+	var MyInt int = 20
+	fmt.Println(MyInt)
+
+	// It is possible to use some specific words from language as a variable name. Don't do that.
+	var any int = 25
+	fmt.Println(any)
+
+	var bool int = 30
+	fmt.Println(bool)
+}
