65fe75dd5c80e1026e9b40e5c22551c090de9a01
Eugene Makarikhin <contact@aboutslack.info>
2026-02-17 11:48:41 +0100

    10_fyne_and_go Added the basic fyne-md app

---
+++ 10_fyne_and_go/02-fyne-md/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct {
+	EditWidget    *widget.Entry
+	PreivewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+	// create a widnow for the app
+	win := a.NewWindow("Markdown")
+
+	// get the user interface
+	edit, preview := cfg.makeUI()
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreivewWidget = preview
+
+	// event listener
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
+}
