3e3d24823b8cadd20ce5ca4edf54d2babc5fda22
Eugene Makarikhin <contact@aboutslack.info>
2026-02-17 15:20:37 +0100

    10_fyne_and_go Added saveAs function

--- 10_fyne_and_go/02-fyne-md/main.go
+++ 10_fyne_and_go/02-fyne-md/main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -24,6 +25,7 @@ func main() {
 
 	// get the user interface
 	edit, preview := cfg.makeUI()
+	cfg.createMenuItems(win)
 
 	// set the content of the window
 	win.SetContent(container.NewHSplit(edit, preview))
@@ -44,4 +46,53 @@ func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
 	edit.OnChanged = preview.ParseMarkdown
 
 	return edit, preview
+}
+
+func (app *config) createMenuItems(win fyne.Window) {
+	// create three menu items
+	openMenuItem := fyne.NewMenuItem("Open...", func() {
+
+	})
+
+	saveMenuItem := fyne.NewMenuItem("Save", func() {
+	})
+
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+
+	// create a file menu, and add the three items to it
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	// create a main menu, and add the file menu to it
+	menu := fyne.NewMainMenu(fileMenu)
+
+	// set the main menu for the application
+	win.SetMainMenu(menu)
+}
+
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			if write == nil {
+				//user cancelled
+				return
+			}
+
+			// save file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " _ " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+		}, win)
+		saveDialog.Show()
+	}
 }
