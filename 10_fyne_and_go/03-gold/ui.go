74096b1e3f77aeeff7b56af75ad3530c3a6b334d
Eugene Makarikhin <contact@aboutslack.info>
2026-02-19 21:23:29 +0100

    10_fyne_and_go Added text for prices on main window

--- 10_fyne_and_go/03-gold/ui.go
+++ 10_fyne_and_go/03-gold/ui.go
@@ -1,10 +1,18 @@
 package main
 
+import "fyne.io/fyne/v2/container"
+
 func (app *Config) makeUI() {
 	// get the current price of gold
+	openPrice, currentPrice, priceChange := app.getPriceText()
 
 	// put price information intro a container
+	priceContent := container.NewGridWithColumns(3, openPrice, currentPrice, priceChange)
+	app.PriceContainer = priceContent
 
 	// add container to window
+	finalContent := container.NewVBox(priceContent)
+
+	app.MainWindow.SetContent(finalContent)
 
 }
