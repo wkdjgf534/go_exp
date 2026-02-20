41706af23774ca1f4d0ed84750bc354c13cf2876
Eugene Makarikhin <contact@aboutslack.info>
2026-02-20 12:31:22 +0100

    10_fyne_and_go Added tabs

--- 10_fyne_and_go/03-gold/ui.go
+++ 10_fyne_and_go/03-gold/ui.go
@@ -1,6 +1,10 @@
 package main
 
-import "fyne.io/fyne/v2/container"
+import (
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+)
 
 func (app *Config) makeUI() {
 	// get the current price of gold
@@ -14,8 +18,15 @@ func (app *Config) makeUI() {
 	toolBar := app.getToolBar()
 	app.ToolBar = toolBar
 
+	// get app tabs
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Prices", theme.HomeIcon(), canvas.NewText("Price content goes here", nil)),
+		container.NewTabItemWithIcon("Holdings", theme.InfoIcon(), canvas.NewText("Holdings content goes here", nil)),
+	)
+	tabs.SetTabLocation(container.TabLocationTop)
+
 	// add container to window
-	finalContent := container.NewVBox(priceContent, toolBar)
+	finalContent := container.NewVBox(priceContent, toolBar, tabs)
 
 	app.MainWindow.SetContent(finalContent)
 
