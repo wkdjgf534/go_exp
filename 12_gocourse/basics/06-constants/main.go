5175f04f3f58a53036b6232f7dc2aeaad08fffcc
Eugene Makarikhin <contact@aboutslack.info>
2026-03-15 15:00:44 +0100

    12_gocourse basic Added 06-constants

---
+++ 12_gocourse/basics/06-constants/main.go
@@ -0,0 +1,18 @@
+package main
+
+const pi = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const days int = 7
+
+	const (
+		monday        = 1
+		tuesday       = 2
+		weneesday     = 3
+		thursday  int = 4
+	)
+
+	// name := "john"
+
+}
