5c2947dbf17be7bd755b168a966d390561635399
Eugene Makarikhin <contact@aboutslack.info>
2026-03-15 13:26:11 +0100

    12_gocourse basic Added 04-variables

---
+++ 12_gocourse/basics/04-variables/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "Peter"
+	// var name1 = "Jane"
+
+	// count := 10
+	// lastName := "Smith"
+
+	//middleName := "Razor"
+	fmt.Println(middleName)
+
+	// Default values
+	// Numeric Types: 0
+	// Boolean Typpes: false
+	// String Type: ""
+	//  Pointers, slices, functions, and structs:
+
+	// ..... SCOPE
+
+	// fmt.Println(firstName)
+}
+
+func printName() {
+	firstName := "Mike"
+	fmt.Println(firstName)
+}
