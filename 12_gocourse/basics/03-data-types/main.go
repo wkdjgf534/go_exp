37208da8a5ab98db2f9efb62a915c73fc236ecb4
Eugene Makarikhin <contact@aboutslack.info>
2026-03-15 10:26:27 +0100

    12_gocourse basic Added 03-data-types

---
+++ 12_gocourse/basics/03-data-types/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello" + "World")
+	fmt.Println("9 x 10", 9*10)
+	fmt.Println("100.18 / 2.0", 100.18/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
