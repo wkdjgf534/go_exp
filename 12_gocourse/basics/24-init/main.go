1e3f849606b8d0a3a0e141ce0aa3e47631d7eb75
Eugene Makarikhin <contact@aboutslack.info>
2026-03-25 13:27:38 +0100

    12_gocourse basic Added 24-init

---
+++ 12_gocourse/basics/24-init/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializing package1...")
+}
+
+func init() {
+	fmt.Println("Initializing package2...")
+}
+
+func init() {
+	fmt.Println("Initializing package3...")
+}
+
+func main() {
+	fmt.Println("Inside the main function")
+}
