d582e3b2de50974ecfbbbae25e596ce18af97267
Eugene Makarikhin <contact@aboutslack.info>
2026-05-21 13:05:12 +0200

    09_go_gof 01-go-breeders Added test template

--- 09_go_gof/01-go-breeders/cmd/web/handlers.go
+++ 09_go_gof/01-go-breeders/cmd/web/handlers.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"go-breeders/pets"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/tsawler/toolbox"
 )
 
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +16,14 @@ func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
 func (app *application) ShowPage(w http.ResponseWriter, r *http.Request) {
 	page := chi.URLParam(r, "page")
 	app.render(w, fmt.Sprintf("%s.page.gohtml", page), nil)
+}
+
+func (app *application) CreateDogFromFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("dog"))
+}
+
+func (app *application) CreateCatFromFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("cat"))
 }
--- 09_go_gof/01-go-breeders/go.mod
+++ 09_go_gof/01-go-breeders/go.mod
@@ -2,4 +2,7 @@ module go-breeders
 
 go 1.26.3
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/tsawler/toolbox v1.3.1 // indirect
+)
