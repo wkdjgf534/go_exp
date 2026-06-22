43089d8e0d743468bcff855faf06acf79ba1284b
Eugene Makarikhin <contact@aboutslack.info>
2026-06-23 00:01:36 +0200

    09_go_gof 07-wp Added comments

--- 09_go_gof/07-wp/streamer/pool.go
+++ 09_go_gof/07-wp/streamer/pool.go
@@ -1,5 +1,6 @@
 package streamer
 
+// VideoDispatcher holds info for a dispatcher.
 type VideoDispatcher struct {
 	WorkerPool chan chan VideoProcessingJob
 	maxWorkers int
@@ -7,14 +8,17 @@ type VideoDispatcher struct {
 	Processor  Processor
 }
 
-// type videoWorker
+// videoWorker holds info for a pool worker. It has the numeric id of the worker,
+// the job queue, and the worker pool chan. A chan chan is used when the thing you want to
+// send down a channel is another channel to send things back.
+// See http://tleyden.github.io/blog/2013/11/23/understanding-chan-chans-in-go/
 type videoWorker struct {
 	id         int
 	jobQueue   chan VideoProcessingJob
 	workerPool chan chan VideoProcessingJob
 }
 
-// newVideoWorker
+// newVideoWorker takes a numeric id and a channel of chan VideoProcessingJob, and returns a videoWorker object.
 func newVideoWorker(id int, workerPool chan chan VideoProcessingJob) videoWorker {
 	return videoWorker{
 		id:         id,
@@ -23,7 +27,7 @@ func newVideoWorker(id int, workerPool chan chan VideoProcessingJob) videoWorker
 	}
 }
 
-// start - starts a worker
+// start starts an individual worker.
 func (w videoWorker) start() {
 	go func() {
 		for {
@@ -39,7 +43,8 @@ func (w videoWorker) start() {
 	}()
 }
 
-// Run()
+// Run runs the workers, and makes the worker pool active. Once we run
+// everything, we can push jobs onto the channel to process them.
 func (vd *VideoDispatcher) Run() {
 	for i := 0; i < vd.maxWorkers; i++ {
 		worker := newVideoWorker(i+1, vd.WorkerPool)
@@ -49,7 +54,7 @@ func (vd *VideoDispatcher) Run() {
 	go vd.dispatch()
 }
 
-// dispatch()
+// dispatch dispatches a worker to handle a job.
 func (vd *VideoDispatcher) dispatch() {
 	for {
 		// Wait for a job to come in.
@@ -62,7 +67,7 @@ func (vd *VideoDispatcher) dispatch() {
 	}
 }
 
-// processVideoJob
+// processVideoJob processes the main queue job with a particular worker.
 func (w videoWorker) processVideoJob(video Video) {
 	video.encode()
 }
--- 09_go_gof/07-wp/streamer/stramer.go
+++ 09_go_gof/07-wp/streamer/stramer.go
@@ -1,5 +1,6 @@
 package streamer
 
+// ProcessingMessage is the information sent back to the client.
 type ProcessingMessage struct {
 	ID         int
 	Successful bool
@@ -7,6 +8,9 @@ type ProcessingMessage struct {
 	OutputFile string
 }
 
+// VideoProcessingJob is the unit of work to be performed. We wrap this type
+// around a Video, which has all the information we need about the input source
+// and what we want the output to look like.
 type VideoProcessingJob struct {
 	Video Video
 }
@@ -15,6 +19,7 @@ type Processor struct {
 	Engine Encoder
 }
 
+// Video is the type for a video that we wish to process.
 type Video struct {
 	ID           int
 	InputFile    string
@@ -29,6 +34,7 @@ func (v *Video) encode() {
 
 }
 
+// New creates and returns a new worker pool.
 func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
 	workerPool := make(chan chan VideoProcessingJob, maxWorkers)
 
