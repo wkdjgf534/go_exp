9233fb4674c9e9afd1ea7bb4bce8d74e3cda7ed5
Eugene Makarikhin <contact@aboutslack.info>
2026-06-22 23:57:07 +0200

    Set up pool functionality

--- 09_go_gof/07-wp/streamer/pool.go
+++ 09_go_gof/07-wp/streamer/pool.go
@@ -6,3 +6,63 @@ type VideoDispatcher struct {
 	jobQueue   chan VideoProcessingJob
 	Processor  Processor
 }
+
+// type videoWorker
+type videoWorker struct {
+	id         int
+	jobQueue   chan VideoProcessingJob
+	workerPool chan chan VideoProcessingJob
+}
+
+// newVideoWorker
+func newVideoWorker(id int, workerPool chan chan VideoProcessingJob) videoWorker {
+	return videoWorker{
+		id:         id,
+		jobQueue:   make(chan VideoProcessingJob),
+		workerPool: workerPool,
+	}
+}
+
+// start - starts a worker
+func (w videoWorker) start() {
+	go func() {
+		for {
+			// Add jobQueue to the worker pool
+			w.workerPool <- w.jobQueue
+
+			// Wait for a job to come back
+			job := <-w.jobQueue
+
+			// Process the job
+			w.processVideoJob(job.Video)
+		}
+	}()
+}
+
+// Run()
+func (vd *VideoDispatcher) Run() {
+	for i := 0; i < vd.maxWorkers; i++ {
+		worker := newVideoWorker(i+1, vd.WorkerPool)
+		worker.start()
+	}
+
+	go vd.dispatch()
+}
+
+// dispatch()
+func (vd *VideoDispatcher) dispatch() {
+	for {
+		// Wait for a job to come in.
+		job := <-vd.jobQueue
+
+		go func() {
+			workerJobQueue := <-vd.WorkerPool
+			workerJobQueue <- job
+		}()
+	}
+}
+
+// processVideoJob
+func (w videoWorker) processVideoJob(video Video) {
+	video.encode()
+}
